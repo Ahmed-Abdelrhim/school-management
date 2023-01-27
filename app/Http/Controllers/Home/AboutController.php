431329c99858331748e23d1a75ac204171cafe2e
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-27 19:17:37 +0200

    update

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -23,9 +23,12 @@ class AboutController extends Controller
 
     public function UpdateAbout(Request $request)
     {
+        $about_id = 1;
+        if (!empty($request->id))
+            $about_id =  $request->id;
+        $about = About::query()->find($about_id);
 
-        $about_id = $request->id;
-
+        $save_url = null;
         if ($request->file('about_image')) {
             $image = $request->file('about_image');
             $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();  // 3434343443.jpg
@@ -33,38 +36,71 @@ class AboutController extends Controller
             Image::make($image)->resize(523, 605)->save('upload/home_about/' . $name_gen);
             $save_url = 'upload/home_about/' . $name_gen;
 
-            About::findOrFail($about_id)->update([
+            //            About::findOrFail($about_id)->update([
+            //                'title' => $request->title,
+            //                'short_title' => $request->short_title,
+            //                'short_description' => $request->short_description,
+            //                'long_description' => $request->long_description,
+            //                'about_image' => $save_url,
+            //
+            //            ]);
+
+            //            $notification = array(
+            //                'message' => 'About Page Updated with Image Successfully',
+            //                'alert-type' => 'success'
+            //            );
+
+            // return redirect()->back()->with($notification);
+
+        }
+        //        else {
+        //
+        //            About::findOrFail($about_id)->update([
+        //                'title' => $request->title,
+        //                'short_title' => $request->short_title,
+        //                'short_description' => $request->short_description,
+        //                'long_description' => $request->long_description,
+        //
+        //            ]);
+        //            $notification = array(
+        //                'message' => 'About Page Updated without Image Successfully',
+        //                'alert-type' => 'success'
+        //            );
+        //
+        //            return redirect()->back()->with($notification);
+        //
+        //        } // end Else
+
+
+        if ($about) {
+            About::query()->update([
                 'title' => $request->title,
                 'short_title' => $request->short_title,
                 'short_description' => $request->short_description,
                 'long_description' => $request->long_description,
                 'about_image' => $save_url,
-
             ]);
             $notification = array(
-                'message' => 'About Page Updated with Image Successfully',
+                'message' => 'About Page Updated Successfully',
                 'alert-type' => 'success'
             );
 
             return redirect()->back()->with($notification);
-
         } else {
-
-            About::findOrFail($about_id)->update([
+            About::query()->create([
                 'title' => $request->title,
                 'short_title' => $request->short_title,
                 'short_description' => $request->short_description,
                 'long_description' => $request->long_description,
-
+                'about_image' => $save_url,
             ]);
             $notification = array(
-                'message' => 'About Page Updated without Image Successfully',
+                'message' => 'About Page Created Successfully',
                 'alert-type' => 'success'
             );
 
             return redirect()->back()->with($notification);
-
-        } // end Else
+        }
 
     } // End Method
 
